c77d13ae2c1f26d398ed5c3707f51609b3e1eaf7
Gene Hsu <gene@hsufarm.com>
2012-02-16 22:23:35 -0800

    change underlying datastructure to be Numeric (Fixnum/Bignum)

--- lib/bitarray.rb
+++ lib/bitarray.rb
@@ -2,26 +2,27 @@ class BitArray
   attr_reader :size
   include Enumerable
   VERSION = "0.0.5"
-  ELEMENT_WIDTH = 32
 
   def initialize(size, default_value = 0)
     @size = size
-    @field = Array.new(((size - 1) / ELEMENT_WIDTH) + 1, 0)
-    @field.map!{|i| ~i} if (default_value == 1)
+    @field = default_value == 0 ? 0 : (1<<size)-1
   end
 
   # Set a bit (1/0)
   def []=(position, value)
-    if value == 1
-      @field[position / ELEMENT_WIDTH] |= 1 << (position % ELEMENT_WIDTH)
-    elsif (@field[position / ELEMENT_WIDTH]) & (1 << (position % ELEMENT_WIDTH)) != 0
-      @field[position / ELEMENT_WIDTH] ^= 1 << (position % ELEMENT_WIDTH)
+    case value
+    when 0
+      @field ^= 1<<position if @field[position]==1
+    when 1
+      @field |= 1<<position
+    else
+      # Undefined, do nothing?
     end
   end
 
   # Read a bit (1/0)
   def [](position)
-    @field[position / ELEMENT_WIDTH] & 1 << (position % ELEMENT_WIDTH) > 0 ? 1 : 0
+    @field[position]
   end
 
   # Iterate over each bit
@@ -31,12 +32,13 @@ class BitArray
 
   # Returns the field as a string like "0101010100111100," etc.
   def to_s
-    @field.collect{|ea| ("%032b" % ea).reverse}.join[0..@size-1]
+    s = ''
+    @size.times { |i| s<<@field[i]+48 }
+    s
   end
 
   # Returns the total number of bits that are set
-  # (The technique used here is about 6 times faster than using each or inject direct on the bitfield)
   def total_set
-    @field.inject(0) { |a, byte| a += byte & 1 and byte >>= 1 until byte == 0; a }
+    (0...@size).inject(0) { |a,i| a + @field[i] }
   end
 end
