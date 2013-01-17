e24928a72f8f77580724374cfe829f1796d31067
Cristiano Betta <cbetta@gmail.com>
2013-01-17 03:31:22 +0000

    Expose field attribute

--- lib/bitarray.rb
+++ lib/bitarray.rb
@@ -1,14 +1,15 @@
 class BitArray
   attr_reader :size
+  attr_reader :field
   include Enumerable
-  
+
   ELEMENT_WIDTH = 32
-  
+
   def initialize(size)
     @size = size
     @field = Array.new(((size - 1) / ELEMENT_WIDTH) + 1, 0)
   end
-  
+
   # Set a bit (1/0)
   def []=(position, value)
     if value == 1
@@ -17,22 +18,22 @@ class BitArray
       @field[position / ELEMENT_WIDTH] ^= 1 << (position % ELEMENT_WIDTH)
     end
   end
-  
+
   # Read a bit (1/0)
   def [](position)
     @field[position / ELEMENT_WIDTH] & 1 << (position % ELEMENT_WIDTH) > 0 ? 1 : 0
   end
-  
+
   # Iterate over each bit
   def each(&block)
     @size.times { |position| yield self[position] }
   end
-  
+
   # Returns the field as a string like "0101010100111100," etc.
   def to_s
     inject("") { |a, b| a + b.to_s }
   end
-  
+
   # Returns the total number of bits that are set
   # (The technique used here is about 6 times faster than using each or inject direct on the bitfield)
   def total_set
